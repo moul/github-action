d51a697f621d2ad49b04d67d4b5a082dc1f26304
Manfred Touron <94029+moul@users.noreply.github.com>
2020-06-29 17:15:43 +0200

    fix: use last CLI api

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM ruby:2.6
+RUN gem install bump-cli
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/sh -l
 
-cd /home
-gem install bump-cli
-$GEM_HOME/bin/bump $1 ${GITHUB_WORKSPACE}/$2 --id $3 --token $4 --validation $5
+$GEM_HOME/bin/bump $1 ${GITHUB_WORKSPACE}/$2 --doc $3 --token $4 --validation $5
